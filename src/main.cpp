236c09064f855bcd53a142ed08614f8716b89711
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-07-06 23:52:39 +0200

    serial optimization

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "SerialGFXInterface_atCPU.hpp"
 
 halvoeGPU::atCPU::SerialGFXInterface g_gpuInterface(Serial8);
+bool g_shouldUpdate = true;
 elapsedMillis interval;
 uint16_t rectX = 5;
 
@@ -19,15 +20,20 @@ void setup()
 
 void loop()
 {
-  if (interval >= 1000)
+  if (interval >= 10)
   {
-    rectX = rectX + 5;
+    rectX = rectX + 1;
     if (rectX == 320 - 25) { rectX = 5; }
-
+    g_shouldUpdate = true;
     interval = 0;
   }
 
-  g_gpuInterface.sendFillScreen();
-  g_gpuInterface.sendFillRect(rectX, 5, 25, 25, 255);
-  g_gpuInterface.sendSwap();
+  if (g_shouldUpdate)
+  {
+    g_gpuInterface.sendFillScreen();
+    g_gpuInterface.sendFillRect(rectX, 5, 25, 25, 255);
+    g_gpuInterface.sendSwap();
+
+    g_shouldUpdate = false;
+  }
 }
